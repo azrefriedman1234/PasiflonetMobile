d62bafe1225b60c9ca7fbff9447a4e7ea8b7a6b8
azrefriedman1234 <azrefriedman@gmail.com>
2026-01-02 12:18:10 +0200

    Final attempt: Ultra-stable build settings

--- app/src/main/java/com/pasiflon/mobile/MainActivity.kt
+++ app/src/main/java/com/pasiflon/mobile/MainActivity.kt
@@ -2,17 +2,12 @@ package com.pasiflon.mobile
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
-import android.view.Gravity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tv = TextView(this).apply {
-            text = "SYSTEM ACTIVE\nPasiflon Mobile"
-            setTextColor(0xFF00F2FF.toInt())
-            textSize = 30f
-            gravity = Gravity.CENTER
-        }
+        val tv = TextView(this)
+        tv.text = "PASIFLON ONLINE"
         setContentView(tv)
     }
 }
